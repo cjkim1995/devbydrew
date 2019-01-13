547086784f3fed07ea9ec7584d8ad82a5356ee3b
CJ Kim <piggy7777@gmail.com>
2019-01-13 13:23:52 -0800

    removed useless code

--- 20190106/20190106.py
+++ 20190106/20190106.py
@@ -1,22 +1,5 @@
 from math import factorial
 
-def factorize(num):
-  facts = []
-
-  while num % 2 == 0:
-    facts.append(2)
-    num /= 2
-
-  for odd in range(3, int(num ** 0.5) + 1, 2):
-    while num % odd == 0:
-      facts.append(odd)
-      num /= odd
-
-  if num >= 2:
-    facts.append(num)
-
-  return facts
-
 
 def trailing_zeroes(number):
   new_num = factorial(number)
